68c62b7c0a3f4767c44a14257adfce18448b4b99
tosslife@sanrenx.com <tosslife>
2014-09-23 11:03:20 +0800

    some update

--- src/com/srx/flytxtview/FlyTxtView.java
+++ src/com/srx/flytxtview/FlyTxtView.java
@@ -1,6 +1,7 @@
 package com.srx.flytxtview;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -16,26 +17,38 @@ public class FlyTxtView extends ViewGroup {
 
 	private static final String TAG = "FlyTxtView";
 	private final static int VIEW_MARGIN = 2;
+	private int textColor = Color.BLACK;
+	private int textSize = 14;
+	private AnimationSet animationSet;
+	private LayoutAnimationController layoutAnimationController;
 
 	public FlyTxtView(Context context) {
-		super(context);
+		this(context, null);
 
 	}
 
 	public FlyTxtView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+		this(context, attrs, 0);
 
 	}
 
 	public FlyTxtView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		init();
+	}
 
+	// init
+	public void init() {
+		animationSet = new AnimationSet(true);
+		layoutAnimationController = new LayoutAnimationController(animationSet, 0.3f);
+		layoutAnimationController.setOrder(LayoutAnimationController.ORDER_NORMAL);
+		setAnimation(null);
+		setLayoutAnimation(layoutAnimationController);
 	}
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		Log.d(TAG, "widthMeasureSpec = " + widthMeasureSpec
-				+ " heightMeasureSpec" + heightMeasureSpec);
+		// Log.d(TAG, "widthMeasureSpec = " + widthMeasureSpec + " heightMeasureSpec" + heightMeasureSpec);
 
 		for (int index = 0; index < getChildCount(); index++) {
 			final View child = getChildAt(index);
@@ -47,11 +60,9 @@ public class FlyTxtView extends ViewGroup {
 	}
 
 	@Override
-	protected void onLayout(boolean changed, int left, int top, int right,
-			int bottom) {
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
 
-		Log.d(TAG, "changed = " + changed + " left = " + left + " top = " + top
-				+ " right = " + right + " botom = " + bottom);
+		// Log.d(TAG, "changed = " + changed + " left = " + left + " top = " + top + " right = " + right + " botom = " + bottom);
 		final int count = getChildCount();
 		int row = 0;// which row lay you view relative to parent
 		int lengthX = left; // right position of child relative to parent
@@ -67,8 +78,7 @@ public class FlyTxtView extends ViewGroup {
 			if (lengthX > right) {
 				lengthX = width + VIEW_MARGIN + left;
 				row++;
-				lengthY = row * (height + VIEW_MARGIN) + VIEW_MARGIN + height
-						+ top;
+				lengthY = row * (height + VIEW_MARGIN) + VIEW_MARGIN + height + top;
 			}
 
 			child.layout(lengthX - width, lengthY - height, lengthX, lengthY);
@@ -76,7 +86,7 @@ public class FlyTxtView extends ViewGroup {
 
 	}
 
-	// set text
+	// set content texts
 	public void setTexts(String text) {
 		Log.v(TAG, text + "");
 		removeAllViews();
@@ -85,31 +95,51 @@ public class FlyTxtView extends ViewGroup {
 		for (int i = 0; i < count; i++) {
 			Log.v(TAG, chars[i] + "");
 			TextView tv = new TextView(getContext());
-			tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.WRAP_CONTENT));
+			tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 			tv.setText(chars[i] + "");
 			tv.setTextSize(18);
+			tv.setTextColor(textColor);
+			tv.setTextSize(textSize);
 			addView(tv);
 		}
 	}
 
+	// config must before setTexts
+	// set texts color
+	public void setTextColor(int textColor) {
+		this.textColor = textColor;
+
+	}
+
+	// config must before setTexts
+	// set text size
+	public void setTextSize(int textSize) {
+		this.textSize = textSize;
+	}
+
+	public void setAnimation(Animation animation) {
+		if (null != animation) {
+			animationSet.addAnimation(animation);
+		} else {
+			setDefaultAnimation();
+		}
+
+	}
+
+	// start animation
 	public void startAnimation() {
-		AnimationSet set = new AnimationSet(true);
+		startLayoutAnimation();
+	}
+
+	// default animation
+	private void setDefaultAnimation() {
+
 		Animation animation = new AlphaAnimation(0.0f, 1.0f);
 		animation.setDuration(300);
-		set.addAnimation(animation);
-
-		animation = new TranslateAnimation(
-				Animation.RELATIVE_TO_PARENT,1,
-				Animation.RELATIVE_TO_SELF,0,
-				Animation.RELATIVE_TO_SELF,0,
-				Animation.RELATIVE_TO_SELF,0);
+		animationSet.addAnimation(animation);
+		animation = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 1, Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0);
 		animation.setDuration(500);
-		set.addAnimation(animation);
-		LayoutAnimationController controller = new LayoutAnimationController(
-				set, 0.5f);
-		//controller.setDelay(1);
-		setLayoutAnimation(controller);
+		animationSet.addAnimation(animation);
 	}
 
 }
--- src/com/srx/flytxtview/MainActivity.java
+++ src/com/srx/flytxtview/MainActivity.java
@@ -1,24 +1,42 @@
 package com.srx.flytxtview;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private FlyTxtView flyTxtView;
+	private TextView resetBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setupViews();
+		setupListensers();
 	}
-	
-	protected void setupViews(){
-		flyTxtView=(FlyTxtView)findViewById(R.id.fly_txt);
+
+	protected void setupViews() {
+		resetBtn = (TextView) findViewById(R.id.btn_reset);
+		flyTxtView = (FlyTxtView) findViewById(R.id.fly_txt);
+		flyTxtView.setTextSize(20);
+		flyTxtView.setTextColor(Color.GREEN);
 		flyTxtView.setTexts("我是一只小小小小鸟,想要飞呀飞 却飞也飞不高,我寻寻觅觅寻寻觅觅一个温暖的怀抱,这样的要求算不算太高");
 		flyTxtView.startAnimation();
 	}
 
+	protected void setupListensers() {
+		resetBtn.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				flyTxtView.setTexts("我是一只来自北方的狼");
+				flyTxtView.startAnimation();
+			}
+		});
+	}
 
 }
